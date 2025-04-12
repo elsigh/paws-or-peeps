db35f56e0c4b0be09586d7007109019489a5f757
elsigh <elsigh@gmail.com>
2025-04-12 08:50:47 -0700

    more ts no diice

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "4mb",
