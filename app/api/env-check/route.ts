16e192b09f7a1da512625c7433ed1c8a29ee1098
elsigh <elsigh@gmail.com>
2025-04-13 08:03:07 -0700

    kill it all with fire

--- app/api/env-check/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-import { getEnvironmentStatus } from "@/lib/env-checker"
-
-export async function GET() {
-  const envStatus = getEnvironmentStatus()
-
-  return NextResponse.json(envStatus)
-}
