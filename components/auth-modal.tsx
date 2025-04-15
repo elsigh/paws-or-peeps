02010ee0f9a604da0f7b4feef13a58da76d34ba4
elsigh <elsigh@gmail.com>
2025-04-15 15:24:56 -0700

    redirectTo is back

--- components/auth-modal.tsx
+++ components/auth-modal.tsx
@@ -41,9 +41,9 @@ export function AuthModal({
 
       const { error } = await supabase.auth.signInWithOAuth({
         provider,
-        // options: {
-        //   redirectTo: `${window.location.origin}/auth/callback`,
-        // },
+        options: {
+          redirectTo: `${window.location.origin}/auth/callback`,
+        },
       });
 
       if (error) throw error;
