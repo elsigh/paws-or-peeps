39c2adf0f159eb66bffcb13fe1a96ea2a490b947
elsigh <elsigh@gmail.com>
2025-04-19 18:04:36 -0700

    header and vote notifications modal

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { HomeIcon, ImageIcon } from "lucide-react";
+import { ImageIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -8,12 +8,13 @@ export function HomePageLink() {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
   const isGalleryPage = pathname === "/gallery";
+  const isResultsPage = pathname.indexOf("/results") === 0;
 
   return (
     <div className="flex items-center gap-4">
       {!isHomePage && (
         <Link href="/" className="text-gray-600 hover:text-gray-900">
-          <HomeIcon className="h-5 w-5" />
+          <PlusIcon className="h-5 w-5" />
         </Link>
       )}
       {!isGalleryPage && (
@@ -22,7 +23,9 @@ export function HomePageLink() {
           className="text-gray-600 hover:text-gray-900 flex items-center gap-2"
         >
           <ImageIcon className="h-5 w-5" />
-          <span className="text-sm font-medium">Gallery</span>
+          {!isResultsPage && (
+            <span className="text-sm font-medium">Gallery</span>
+          )}
         </Link>
       )}
     </div>
